01a14d40f27f36f6bd97d73e7fd2273c0ab1366a
sobhan-jabbari <sobhan.jabbari@gmail.com>
2023-09-27 13:20:10 +0330

    v1.0.19 upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ task("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
-val appVersionCode by extra(1018)
-val appVersionName by extra("1.0.18")
+val appVersionCode by extra(1019)
+val appVersionName by extra("1.0.19")
 
 
 task("backup", Zip::class) {
